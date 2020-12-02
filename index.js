bc6c4eb9f16211e5469209ae1046ba99430483d4
akashfit2max <36466687+akashfit2max@users.noreply.github.com>
2020-12-02 12:56:41 +0530

    bajate rhoo 🥁

---
+++ index.js
@@ -0,0 +1,62 @@
+var numOfDrums = document.querySelectorAll(".drum").length;
+for(var i=0; i < numOfDrums;i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+        var char = this.innerHTML;
+        makeSound(char);
+        buttonAnimation(char);
+        // console.log(char);
+        
+    })
+}
+
+document.addEventListener("keypress",function(event) {
+    makeSound(event.key);  //used to key the key which is pressed i.e the event which triggers the eventListener
+    buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "a":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "s":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+        case "l":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;        
+        default: console.log(char);
+    }
+}
+
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")
+    }, 100);
+}
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
